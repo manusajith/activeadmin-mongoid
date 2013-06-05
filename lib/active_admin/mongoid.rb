708ec677f91c5c46e65b6eca9fbbb8bfbd54c02e
Manu S Ajith <neo@codingarena.in>
2013-06-06 01:01:58 +0530

    loading monkey patched resources

--- lib/active_admin/mongoid.rb
+++ lib/active_admin/mongoid.rb
@@ -4,22 +4,30 @@ require 'rails'
 require 'mongoid'
 require 'active_admin'
 
-module ActiveAdmin
-  module Mongoid
-end
+# module ActiveAdmin
+#   module Mongoid
+# end
 
-class << self
- alias setup_without_mongoid setup
+require 'active_admin/mongoid/comments'
+require 'active_admin/mongoid/adaptor'
+require 'active_admin/mongoid/filter_form_builder'
+require 'active_admin/mongoid/data_access'
+require 'active_admin/mongoid/document'
+require 'active_admin/mongoid/helpers/collection'
+require 'active_admin/mongoid/criteria'
 
- # Load monkey patches *after* the setup process
-  def setup *args, &block
-    setup_without_mongoid *args, &block
-    require 'active_admin/mongoid/comments'
-    require 'active_admin/mongoid/adaptor'
-    require 'active_admin/mongoid/filter_form_builder'
-    require 'active_admin/mongoid/data_access'
-    require 'active_admin/mongoid/document'
-    require 'active_admin/mongoid/helpers/collection'
-    require 'active_admin/mongoid/criteria'
-  end
+# class << self
+#  alias setup_without_mongoid setup
+
+#  # Load monkey patches *after* the setup process
+#   def setup *args, &block
+#     setup_without_mongoid *args, &block
+#     require 'active_admin/mongoid/comments'
+#     require 'active_admin/mongoid/adaptor'
+#     require 'active_admin/mongoid/filter_form_builder'
+#     require 'active_admin/mongoid/data_access'
+#     require 'active_admin/mongoid/document'
+#     require 'active_admin/mongoid/helpers/collection'
+#     require 'active_admin/mongoid/criteria'
+#   end
 end
